15f33d9b70b09af07886fa45a370f6db06fe57b3
Pramuditha Karunarathna <shemil076@gmail.com>
2021-10-30 14:59:34 +0530

    Created the prin main menu method

--- src/Formula1ChampionshipManager.java
+++ src/Formula1ChampionshipManager.java
@@ -1,3 +1,35 @@
 public class Formula1ChampionshipManager implements ChampionshipManager{
     private int numberOfDrivers;
+    private int numberOfCars;
+
+
+    public static void main(String[] args) {
+        printMenu();
+    }
+
+
+    public static void printMenu() {
+        System.out.println("     __  __    __    ____  _  _    __  __  ____  _  _  __  __ ");
+        System.out.println("    (  \\/  )  /__\\  (_  _)( \\( )  (  \\/  )( ___)( \\( )(  )(  )");
+        System.out.println("     )    (  /(__)\\  _)(_  )  (    )    (  )__)  )  (  )(__)( ");
+        System.out.println("    (_/\\/\\_)(__)(__)(____)(_)\\_)  (_/\\/\\_)(____)(_)\\_)(______)");
+
+        System.out.println("");
+        System.out.println("+----------------------------------------------------------------+");
+        System.out.println("|      INPUT CODE      |              OPTION                     |");
+        System.out.println("+----------------------------------------------------------------+");
+        System.out.println("|      100 or CND      |       Create a New Driver               |");
+        System.out.println("|      101 or DDT      |       Delete a Driver and Team          |");
+        System.out.println("|      102 or CTD      |       Change the Driver                 |");
+        System.out.println("|      103 or DVS      |       Display the Various Statics       |");
+        System.out.println("|      104 or DFT      |       Display Formula1 Driver Table     |");
+        System.out.println("|      105 or ARC      |       Add a Race Completed              |");
+        System.out.println("|      106 or SIF      |       Save in a File                    |");
+        System.out.println("+----------------------------------------------------------------+");
+
+
+    }
+
+
 }
+
