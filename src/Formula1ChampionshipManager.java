402f9b0e48fe9842c46195d0a3e438c846f23205
Pramuditha Karunarathna <shemil076@gmail.com>
2021-11-08 20:11:39 +0530

    :sparkles: implement the deleteADriverAndTeam method

--- src/Formula1ChampionshipManager.java
+++ src/Formula1ChampionshipManager.java
@@ -14,7 +14,7 @@ public class Formula1ChampionshipManager<HashTable> implements ChampionshipManag
     private String[] countryCodes = {"ARG", "AUS", "AUT", "BHR", "BEL", "BRA", "CAN", "CHL", "COL", "CZE", "DNK", "FIN", "FRA", "DEU", "HUN", "IND", "IRL", "ITA", "JPN", "LIE", "MYS", "MEX", "MCO", "MAR", "NLD", "NZL", "POL", "PRT", "RUS", "ZAF", "ESP", "SWE", "CHE", "THA", "GBR", "USA", "URY", "VEN", "ARE"};
     private String[] existingTeamsArray = {"MERCEDES", "RED BULL", "MCLAREN", "FERRARI", "ALPINE", "ALFA TAURI", "ASTON MARTIN", "WILLIAMS", "ALFA ROMEO RACING", "HAAS F1 TEAM"};
     private boolean checkNumber = false, checkCountryCode = false;
-    private boolean checkExistingTeamTeamName = true, checkDisplayTheVariousStatics = true, checkTeamName = true, checkCreateANewDriver = true, vacantTeams = true, occupiedTeam = true, checkCustomOccupiedTeam = true;
+    private boolean checkExistingTeamTeamName = true, checkDisplayTheVariousStatics = true, checkTeamName = true, checkCreateANewDriver = true, vacantTeams = true, occupiedTeam = true, checkCustomOccupiedTeam = true, checkDeleteADriverAndTeam = true;
 
     private int firstPositions, secondPositions, thirdPositions, achievedSeasons, currentPoints, numberOfRaces;
 
@@ -79,6 +79,7 @@ public class Formula1ChampionshipManager<HashTable> implements ChampionshipManag
                     System.out.println("+------------------------------------------------------------------------+");
                     System.out.println("|                         Delete a Driver and Team                       |");
                     System.out.println("+------------------------------------------------------------------------+");
+                    deleteADriverAndTeam();
                     break;
 
                 case "102":
@@ -683,8 +684,6 @@ public class Formula1ChampionshipManager<HashTable> implements ChampionshipManag
                 }
             }
         }
-
-
     }
 
 
@@ -812,6 +811,61 @@ public class Formula1ChampionshipManager<HashTable> implements ChampionshipManag
             System.out.println("Please add a team before this function :-(");
         }
     }
+
+    public void deleteADriverAndTeam(){
+        while (checkDeleteADriverAndTeam) {
+
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("|         INPUT CODE         |              OPTION                       |");
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("|         1 or DBT           |     Delete driver from pre build team     |");
+            System.out.println("|         2 or DCT           |     Delete a diver from custom team       |");
+            System.out.println("|         9 or BCK           |     Return To the main menu               |");
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("Enter the respective code for your requirement: ");
+
+
+            if (input.hasNext()) {
+                String respond = input.next();
+                switch (respond.toUpperCase()) {
+                    case "1":
+                    case "DBT":
+                        System.out.println("+------------------------------------------------------------------------+");
+                        System.out.println("|                 Select a driver from pre build team                    |");
+                        System.out.println("+------------------------------------------------------------------------+");
+
+
+                        break;
+
+                    case "2":
+                    case "DCT":
+                        System.out.println("+------------------------------------------------------------------------+");
+                        System.out.println("|                   Select a diver from custom team                      |");
+                        System.out.println("+------------------------------------------------------------------------+");
+
+
+                        break;
+
+                    case "9":
+                    case "BCK":
+                        checkDeleteADriverAndTeam = false;
+                        break;
+                    default:
+                        System.out.println("Invalid option selected, 'Input out of range'");
+                }
+            } else {
+                System.out.println("Invalid in-put");                                                                      //if the user input an invalid input, the program will not terminate, just repeat by passing a warning
+            }
+        }
+        checkDeleteADriverAndTeam = true;
+    }
+
+
+
+
+
+
+
 
 }
 
